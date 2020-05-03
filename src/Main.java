dcd20afaca618a2312e298ade938571a7ec1cf25
sitihalimatussa'diah <hlmtss18@gmail.com>
2020-05-03 21:15:22 +0700

    Menjalankan Class, Attribute dan Method

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scincare wajah = new Scincare();
+        System.out.println(wajah.brand ="Wardah");
+        System.out.println(wajah.merek ="Serum Wajah");
+        System.out.println(wajah.warna ="Putih Bening");
+        System.out.println(wajah.harga);
+        System.out.println(wajah.exp);
+
+        wajah.cantik();
+    }
+}
