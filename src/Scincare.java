654da4c1261fdb3137615f9bcc54989c12623a86
sitihalimatussa'diah <hlmtss18@gmail.com>
2020-05-03 21:14:20 +0700

    Attribute dan Object

---
+++ src/Scincare.java
@@ -0,0 +1,15 @@
+public class Scincare {
+
+    String brand;
+    String merek;
+    String warna;
+    final int exp =20092022;
+    final int harga =50000;
+
+    void cantik() {
+
+        System.out.println("Serum yang asli berwarna putih Bening");
+        System.out.println("Serum Wardah Ini Bisa Membuat Muka Glowing");
+        System.out.println("Serum Wardah Ini Bisa Menghilangkan Jerawat");
+    }
+}
